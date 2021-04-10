108a302c1c09f2ec667db9dd8bca92d817c377ce
myunghyun_yang <shiningy84@gmail.com>
2021-04-10 13:59:10 +0900

    prod build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,27 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-	purge: [],
-	darkMode: false, // or 'media' or 'class'
-	theme: {
-		truncate: {
-			lines: {
-				3: "3",
-				4: "4",
-				5: "5",
-				8: "8",
-			},
-		},
-		extend: {
-			colors: {
-				indigo: colors.indigo
-			}
-		},
-	},
-	variants: {
-		extend: {
-			display: ["group-hover"],
-		},
-	},
-	plugins: [require("tailwindcss-truncate-multiline")()],
+  purge: ["./src/**/*.tsx"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    truncate: {
+      lines: {
+        3: "3",
+        4: "4",
+        5: "5",
+        8: "8",
+      },
+    },
+    extend: {
+      colors: {
+        indigo: colors.indigo,
+      },
+    },
+  },
+  variants: {
+    extend: {
+      display: ["group-hover"],
+    },
+  },
+  plugins: [require("tailwindcss-truncate-multiline")()],
 };
